2fab1450cee19b0942aee2f7076e24429ba74ca8
Jhun24 <jjanghuni102@naver.com>
2018-08-29 14:04:21 +0900

    viewpager test

--- public/js/viewpager.js
+++ public/js/viewpager.js
@@ -4,6 +4,11 @@
 
 let view_pagrer = document.getElementById('view-pager');
 
+let view_place_translateX = 0;
+
+let startX = 0;
+let finishX = 0;
+
 flower_btn.addEventListener('click',function () {
     animate(0);
 });
@@ -20,14 +25,32 @@ setting_btn.addEventListener('click',function () {
     animate(3);
 });
 
-view_pagrer.addEventListener('touchmove',function (event) {
-    console.log(event);
-    document.getElementById('check').innerHTML = event;
+view_pagrer.addEventListener('touchstart',function (e) {
+    startX = e.targetTouches[0].screenX;
 });
+
+view_pagrer.addEventListener('touchend',function (e) {
+    finishX = e.changedTouches[0].screenX;
+
+    if(finishX - startX > 89 ){
+        if(view_place_translateX != -75){
+            view_place_translateX = view_place_translateX - 25;
+            view_pagrer.style.transitionDuration = '1s';
+            view_pagrer.style.transform = "translateX("+view_place_translateX+"%)";
+        }
+    }
+    else if(startX - finishX > 89){
+        if(view_place_translateX != 0){
+            view_place_translateX = view_place_translateX + 25;
+            view_pagrer.style.transitionDuration = '1s';
+            view_pagrer.style.transform = "translateX("+view_place_translateX+"%)";
+        }
+    }
+})
 
 
 function animate(view_place) {
-    let view_place_translateX = view_place * 25 * - 1;
+    view_place_translateX = view_place * 25 * - 1;
     view_pagrer.style.transitionDuration = '1s';
     view_pagrer.style.transform = "translateX("+view_place_translateX+"%)";
 }
