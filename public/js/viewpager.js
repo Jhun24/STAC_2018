581b3d707f3de587c0cf0987e3481925bcb13918
Jhun24 <jjanghuni102@naver.com>
2018-08-29 09:17:20 +0900

    view backup

---
+++ public/js/viewpager.js
@@ -0,0 +1,32 @@
+/**
+ * Created by janghunlee on 2018. 8. 28..
+ */
+
+let view_pagrer = document.getElementById('view-pager');
+
+flower_btn.addEventListener('click',function () {
+    animate(0);
+});
+
+mic_btn.addEventListener('click',function () {
+    animate(1);
+});
+
+book_btn.addEventListener('click',function () {
+    animate(2);
+});
+
+setting_btn.addEventListener('click',function () {
+    animate(3);
+});
+
+view_pagrer.addEventListener('touchmove',function (event) {
+    console.log(event);
+});
+
+
+function animate(view_place) {
+    let view_place_translateX = view_place * 25 * - 1;
+    view_pagrer.style.transitionDuration = '1s';
+    view_pagrer.style.transform = "translateX("+view_place_translateX+"%)";
+}
