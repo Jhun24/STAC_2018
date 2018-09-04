5335713e3bae60b46770fec03ed90bdbc3878149
Jhun24 <jjanghuni102@naver.com>
2018-09-04 13:24:16 +0900

    mobile chrome scroll

---
+++ public/js/scroll.js
@@ -0,0 +1,12 @@
+/**
+ * Created by janghunlee on 2018. 9. 4..
+ */
+window.onload = function() {
+    setTimeout(function() {
+        window.scrollTo(0, 1);}, 100);
+};
+
+// addEventListener 이벤트
+window.addEventListener('load', function(){
+    setTimeout(scrollTo, 1);
+}, false);
